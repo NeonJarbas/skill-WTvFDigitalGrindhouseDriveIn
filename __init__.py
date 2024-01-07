91d8de843dcfa192c53ac63c71732209f05fc778
JarbasAi <jarbasai@mailfence.com>
2024-01-07 01:13:40 +0000

    refactor/ocp

--- __init__.py
+++ __init__.py
@@ -1,66 +1,53 @@
-from os.path import join, dirname
 import random
-from ovos_plugin_common_play.ocp import MediaType, PlaybackType
-from ovos_utils.log import LOG
-from ovos_utils.parse import fuzzy_match
-from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
-    ocp_search, ocp_featured_media
-from youtube_archivist import YoutubeMonitor
+from os.path import join, dirname
+
+import requests
+from json_database import JsonStorageXDG
+
+from ovos_utils.ocp import MediaType, PlaybackType
+from ovos_workshop.decorators.ocp import ocp_search, ocp_featured_media
+from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
 
 
 class WTvFDigitalGrindhouseDriveInSkill(OVOSCommonPlaybackSkill):
-    def __init__(self):
-        super().__init__("WTvFDigitalGrindhouseDriveIn")
-        self.supported_media = [MediaType.MOVIE,
-                                MediaType.GENERIC]
+    def __init__(self, *args, **kwargs):
+        self.supported_media = [MediaType.MOVIE]
         self.skill_icon = join(dirname(__file__), "ui", "logo.jpg")
         self.default_bg = self.skill_icon
-        self.archive = YoutubeMonitor(db_name="WTvFDigitalGrindhouseDriveIn",
-                                      blacklisted_kwords=["trailer", "retro cuties",
-                                                          "teaser", "movie scene",
-                                                          "movie clip", "behind the scenes",
-                                                          "Cult Clips", "Movie Preview",
-                                                          "| Presented by DUST",
-                                                          "Fight Scene", "| DUST", "Crypt TV", "Subscribe",
-                                                          "soundtrack", " OST", "opening theme"],
-                                      min_duration=30 * 60,
-                                      logger=LOG)
+        self.archive = JsonStorageXDG("WTvFDigitalGrindhouseDriveIn", subfolder="OCP")
+        super().__init__(*args, **kwargs)
 
     def initialize(self):
-        bootstrap = "https://github.com/JarbasSkills/skill-WTvFDigitalGrindhouseDriveIn/raw/dev/bootstrap.json"
-        self.archive.bootstrap_from_url(bootstrap)
-        self.schedule_event(self._sync_db, random.randint(3600, 24 * 3600))
+        self._sync_db()
+        self.load_ocp_keywords()
+
+    def load_ocp_keywords(self):
+        titles = []
+        genre = ["horror movie","slasher", "exploitation",
+                 "grindhouse","classic movie", "science fiction"]
+
+        for url, data in self.archive.items():
+            t = data["title"]
+            title, year = t.split("(")
+            year = year.split(")")[0]
+            titles.append(title.strip())
+
+        self.register_ocp_keyword(MediaType.MOVIE,
+                                  "movie_name", titles)
+        self.register_ocp_keyword(MediaType.MOVIE,
+                                  "film_genre", genre)
+        self.register_ocp_keyword(MediaType.MOVIE,
+                                  "movie_streaming_provider",
+                                  ["WTvFDigitalGrindhouseDriveIn", "WTvF",
+                                   "Digital Grindhouse", "Grindhouse DriveIn"])
 
     def _sync_db(self):
-        url = "https://www.youtube.com/c/WTvFDigitalGrindhouseDriveIn"
-        self.archive.parse_videos(url)
-        self.schedule_event(self._sync_db, random.randint(3600, 24*3600))
+        bootstrap = "https://github.com/JarbasSkills/skill-WTvFDigitalGrindhouseDriveIn/raw/dev/bootstrap.json"
+        data = requests.get(bootstrap).json()
+        self.archive.merge(data)
+        self.schedule_event(self._sync_db, random.randint(3600, 24 * 3600))
 
-    # matching
-    def match_skill(self, phrase, media_type):
-        score = 0
-        if self.voc_match(phrase, "movie") or media_type == MediaType.MOVIE:
-            score += 10
-        if self.voc_match(phrase, "wtvf"):
-            score += 50
-        return score
-
-    def normalize_title(self, title):
-        title = title.lower().strip()
-        title = self.remove_voc(title, "wtvf")
-        title = self.remove_voc(title, "movie")
-        title = title.replace("|", "").replace('"', "") \
-            .replace(':', "").replace('”', "").replace('“', "") \
-            .strip()
-        return " ".join(
-            [w for w in title.split(" ") if w])  # remove extra spaces
-
-    def calc_score(self, phrase, match, base_score=0):
-        score = base_score
-        score += 100 * fuzzy_match(phrase.lower(), match["title"].lower())
-        return min(100, score)
-
-    def get_playlist(self, score=50, num_entries=250):
+    def get_playlist(self, score=50, num_entries=25):
         return {
             "match_confidence": score,
             "media_type": MediaType.MOVIE,
@@ -75,27 +62,33 @@ class WTvFDigitalGrindhouseDriveInSkill(OVOSCommonPlaybackSkill):
 
     @ocp_search()
     def search_db(self, phrase, media_type):
-        base_score = self.match_skill(phrase, media_type)
-        if self.voc_match(phrase, "wtvf"):
-            if self.voc_match(phrase, "wtvf", exact=True):
-                base_score = 100
-            yield self.get_playlist(base_score)
-        if media_type == MediaType.MOVIE:
-            # only search db if user explicitly requested movies
-            phrase = self.normalize_title(phrase)
-            for url, video in self.archive.db.items():
+        base_score = 15 if media_type == MediaType.MOVIE else 0
+        entities = self.ocp_voc_match(phrase)
+
+        title = entities.get("movie_name")
+        skill = "movie_streaming_provider" in entities  # skill matched
+
+        base_score += 30 * len(entities)
+
+        if title:
+            candidates = [video for video in self.archive.values()
+                          if title.lower() in video["title"].lower()]
+            for video in candidates:
                 yield {
                     "title": video["title"],
-                    "author": "WTvF Digital Grindhouse Drive In",
-                    "match_confidence": self.calc_score(phrase, video, base_score),
+                    "author": video["author"],
+                    "match_confidence": min(100, base_score),
                     "media_type": MediaType.MOVIE,
-                    "uri": "youtube//" + url,
+                    "uri": "youtube//" + video["url"],
                     "playback": PlaybackType.VIDEO,
                     "skill_icon": self.skill_icon,
                     "skill_id": self.skill_id,
                     "image": video["thumbnail"],
                     "bg_image": self.default_bg
                 }
+
+        if skill:
+            yield self.get_playlist()
 
     @ocp_featured_media()
     def featured_media(self):
@@ -109,8 +102,13 @@ class WTvFDigitalGrindhouseDriveInSkill(OVOSCommonPlaybackSkill):
             "skill_icon": self.skill_icon,
             "bg_image": video["thumbnail"],
             "skill_id": self.skill_id
-        } for video in self.archive.sorted_entries()]
+        } for video in self.archive.values()]
 
 
-def create_skill():
-    return WTvFDigitalGrindhouseDriveInSkill()
+if __name__ == "__main__":
+    from ovos_utils.messagebus import FakeBus
+
+    s = WTvFDigitalGrindhouseDriveInSkill(bus=FakeBus(), skill_id="t.fake")
+    for r in s.search_db("Plan 9 from Outer Space", MediaType.MOVIE):
+        print(r)
+        # {'title': 'Plan 9 from Outer Space (1959) *public domain', 'author': 'WTvF! - Digital Grindhouse & Drive-In', 'match_confidence': 45, 'media_type': <MediaType.MOVIE: 10>, 'uri': 'youtube//https://youtube.com/watch?v=4z6VLR2GA1w', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake', 'image': 'https://i.ytimg.com/vi/4z6VLR2GA1w/sddefault.jpg', 'bg_image': '/home/miro/PycharmProjects/OCP_sprint/skills/skill-WTvFDigitalGrindhouseDriveIn/ui/logo.jpg'}
